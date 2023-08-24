d15085b5f02ee889bb7d1414d378c1256d4dd282
Sriram <184502@sasi.edu.in>
2023-08-24 22:44:00 +0530

    save

--- app.js
+++ app.js
@@ -14,38 +14,22 @@ function displayUsers() {
     users.forEach((user, index) => {
         const userCard = document.createElement('div');
         userCard.classList.add('card');
-        const userIcon = document.createElement('img');
-        userIcon.setAttribute('src', 'https://img.icons8.com/material-sharp/24/user.png');
-        userIcon.setAttribute('alt', 'user');
-        userIcon.setAttribute('width', '24');
-        userIcon.setAttribute('style', 'height: 24px');
-
-        const detailsContainer = document.createElement('div');
-        detailsContainer.classList.add('det');
-
-        const details = document.createElement('div');
-        details.classList.add('details');
 
         const nameParagraph = document.createElement('p');
         nameParagraph.classList.add('nameo');
-        nameParagraph.textContent = user.name;
+        nameParagraph.textContent = 'Name: '+user.name;
 
         const emailParagraph = document.createElement('p');
         emailParagraph.classList.add('emailo');
-        emailParagraph.textContent = user.email;
-
-        details.appendChild(nameParagraph);
-        details.appendChild(emailParagraph);
+        emailParagraph.textContent = 'Email: '+user.email;
 
         const centerParagraph = document.createElement('p');
         centerParagraph.classList.add('center');
-        centerParagraph.textContent = user.city;
+        centerParagraph.textContent = 'center: '+user.city;
 
-        detailsContainer.appendChild(details);
-        detailsContainer.appendChild(centerParagraph);
-
-        userCard.appendChild(userIcon);
-        userCard.appendChild(detailsContainer);
+        userCard.appendChild(nameParagraph);
+        userCard.appendChild(emailParagraph);
+        userCard.appendChild(centerParagraph);
 
         resultsContainer.appendChild(userCard);
     });
@@ -58,7 +42,15 @@ function add() {
         email: email.value,
         city: randomIndianCity      
     }
-    users.push(user);
+    let checkuser = users.filter((useri) => {
+        return user.email == useri.email;
+    });
+    if(checkuser.length == 0){
+        users.push(user);
+    }
+    else{
+        alert('user already registered');
+    }
     displayUsers();
     console.log(users);
 }
